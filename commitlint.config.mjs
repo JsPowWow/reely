ffdbb797fcaae178958a96ea92ef3868fa15b227
JsPowWow <js.developer.powwow@gmail.com>
2026-01-12 15:05:15 +0100

    chore: add commitlint & husky

---
+++ commitlint.config.mjs
@@ -0,0 +1,4 @@
+// https://medium.com/@michadrabikowski/automated-releases-with-nx-and-github-actions-07f574041293
+export default {
+  extends: ['@commitlint/config-conventional', '@commitlint/config-nx-scopes'],
+};
