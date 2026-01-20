44304202e8d05db701a25daf93bd159f805ffe1f
JsPowWow <js.developer.powwow@gmail.com>
2026-01-20 14:51:46 +0100

    feat(dommy): build experiments

--- packages/dommy/vite.config.ts
+++ packages/dommy/vite.config.ts
@@ -1,10 +1,39 @@
 /// <reference types='vitest' />
+import { resolve } from 'path';
+
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig(() => ({
   root: import.meta.dirname,
   cacheDir: '../../node_modules/.vite/packages/dommy',
-  plugins: [],
+  plugins: [
+    dts({
+      entryRoot: 'src',
+      tsconfigPath: './tsconfig.lib.json',
+    }),
+  ],
+  build: {
+    // lib: {
+    //   entry: resolve(import.meta.dirname, 'src/index.ts'),
+    //   formats: ['es'],
+    //   fileName: 'index',
+    // },
+    lib: {
+      entry: resolve(import.meta.dirname, 'src/index.ts'),
+      fileName: 'index',
+      formats: ['es' as const],
+    },
+    minify: true,
+    outDir: 'dist',
+    rollupOptions: {
+      external: ['tslib'],
+      output: {
+        preserveModules: false,
+      },
+    },
+    sourcemap: true,
+  },
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [],
